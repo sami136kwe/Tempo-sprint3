7eba5a11edd0673ffb0bc1c86094ca7625485762
Alexandre Blondin Massé <blondin_masse.alexandre@uqam.ca>
2025-07-27 15:02:17 -0400

    Réusine sous-commandes

--- src/tempo.c
+++ src/tempo.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-#define HELP "Usage: tempo\n\
-Displays information about a timeseries.\n"
+#define HELP "Usage: tempo SUBCOMMAND\n\
+Displays information about a timeseries.\n\
+\n\
+SUBCOMMAND is mandatory and must take one of the following values:\n\
+  describe: describes the timeseries\n\
+  help: shows this message\n"
 
 /**
  * Main function
@@ -12,9 +16,13 @@ Displays information about a timeseries.\n"
  * @return      0 if usage is normal, 1 otherwise
  */
 int main(int argc, char *argv[]) {
-  if (argc == 2 && strcmp(argv[1], "describe") == 0)
-    printf("An empty timeseries with origin 2025-09-01T09:00:00\n");
-  else
-    printf(HELP);
-  return 0;
+  if (argc == 2) {
+    if (strcmp(argv[1], "describe") == 0)
+      printf("An empty timeseries with origin 2025-09-01T09:00:00\n");
+    else if (strcmp(argv[1], "help") == 0)
+      printf(HELP);
+    return 0;
+  }
+  fprintf(stderr, "error: subcommand is mandatory\n");
+  return 1;
 }
