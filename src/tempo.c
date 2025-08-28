e87ec0b4e75c6e05a023bd84b6c767730d2c9643
Alexandre Blondin Massé <blondin_masse.alexandre@uqam.ca>
2025-08-28 13:55:41 -0400

    Réusine tempo.c en introduisant struct Timeseries

--- src/tempo.c
+++ src/tempo.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
+// Constants
+// ---------
+
 #define HELP "Usage: tempo SUBCOMMAND\n\
 Displays information about a timeseries.\n\
 \n\
@@ -10,20 +13,47 @@ SUBCOMMAND is mandatory and must take one of the following values:\n\
 
 // The maximum length of a read line
 #define LINE_MAX_LENGTH 100
-// A fixed datetime
-#define DATETIME "2025-09-01T09:00:00"
+// The length of a datetime
+#define DATETIME_LENGTH 19
+
+// Types
+// -----
+
+// A timeseries
+struct Timeseries {
+  // The start datetime
+  char start_datetime[DATETIME_LENGTH + 1];
+};
+
+// Functions
+// ---------
+
+/**
+ * Set the start datetime of the timeseries from stdin
+ *
+ * @param timeseries  The timeseries to set
+ */
+void set_start_datetime_from_stdin(struct Timeseries* timeseries) {
+  char line[LINE_MAX_LENGTH];
+  if (fgets(line, LINE_MAX_LENGTH, stdin) != NULL) {
+    strncpy(timeseries->start_datetime, line, DATETIME_LENGTH);
+  }
+}
 
 /**
  * Runs the 'describe' subcommand
  */
 void run_describe(void) {
   int num_lines = 0;
+  struct Timeseries timeseries;
+  set_start_datetime_from_stdin(&timeseries);
   char line[LINE_MAX_LENGTH];
   while (fgets(line, LINE_MAX_LENGTH, stdin) != NULL) {
     ++num_lines;
   }
-  printf("Range: [%s, %s]\n", DATETIME, DATETIME);
-  printf("Size: %d\n", num_lines - 1);
+  printf("Range: [%s, %s]\n",
+         timeseries.start_datetime, timeseries.start_datetime);
+  printf("Size: %d\n", num_lines);
 }
 
 /**
@@ -31,12 +61,14 @@ void run_describe(void) {
  */
 void run_show(void) {
   int num_lines = 0;
+  struct Timeseries timeseries;
+  set_start_datetime_from_stdin(&timeseries);
   char line[LINE_MAX_LENGTH];
   while (fgets(line, LINE_MAX_LENGTH, stdin) != NULL) {
     ++num_lines;
   }
-  if (num_lines == 2)
-    printf("%s 10\n", DATETIME);
+  if (num_lines == 1)
+    printf("%s 10\n", timeseries.start_datetime);
 }
 
 /**
