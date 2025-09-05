a0f83580d49544d645b86fe22b77603305f090b7
Alexandre Blondin Massé <blondin_masse.alexandre@uqam.ca>
2025-09-05 08:42:18 -0400

    Ajoute validation horodate invalide

--- bats/test_input.bats
+++ bats/test_input.bats
@@ -6,8 +6,41 @@ setup() {
   examples_dir="$root_dir/examples"
 }
 
+# Wrong datetime
+# --------------
+
 @test "tempo show with datetime missing the character T returns an error" {
   run tempo show < "$examples_dir/missing_t.invalid"
   [ "$status" -eq 2 ]
   assert_output "error: invalid datetime format (should be YYYY:mm:DDTHH:MM:SS)"
+}
+
+@test "tempo show with wrong character in date returns an error" {
+  run tempo show < "$examples_dir/wrong_char_date.invalid"
+  [ "$status" -eq 2 ]
+  assert_output "error: invalid datetime format (should be YYYY:mm:DDTHH:MM:SS)"
+}
+
+@test "tempo show with wrong character in time returns an error" {
+  run tempo show < "$examples_dir/wrong_char_time.invalid"
+  [ "$status" -eq 2 ]
+  assert_output "error: invalid datetime format (should be YYYY:mm:DDTHH:MM:SS)"
+}
+
+@test "tempo show with wrong day returns an error" {
+  run tempo show < "$examples_dir/wrong_day.invalid"
+  [ "$status" -eq 2 ]
+  assert_output "error: invalid datetime (2025-02-29T00:00:00)"
+}
+
+@test "tempo show with wrong month returns an error" {
+  run tempo show < "$examples_dir/wrong_month.invalid"
+  [ "$status" -eq 2 ]
+  assert_output "error: invalid datetime (2025-23-04T00:00:00)"
+}
+
+@test "tempo show with wrong year returns an error" {
+  run tempo show < "$examples_dir/wrong_year.invalid"
+  [ "$status" -eq 2 ]
+  assert_output "error: invalid datetime (5002-06-04T00:00:00)"
 }
--- src/datetime.c
+++ src/datetime.c
@@ -1,5 +1,6 @@
 #include "datetime.h"
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,9 +17,70 @@ void report_memory_problem(const char* context) {
   exit(1);
 }
 
+/**
+ * Indicates if a given datetime is valid
+ *
+ * @param year   The year of the datetime
+ * @param month  The month of the datetime
+ * @param day    The day of the datetime
+ * @param hour   The hour of the datetime
+ * @param min    The minutes of the datetime
+ * @param sec    The seconds of the datetime
+ * @return       true if and only if the datetime is valid
+ */
+bool datetime_is_valid(int year, int month, int day,
+                       int hour, int min, int sec) {
+  if (year < 1900 || year > 3000 ||
+      month < 1 || month > 12 ||
+      day < 1)
+    return false;
+  switch (month) {
+    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+      if (day > 31) return false;
+      break;
+    case 4: case 6: case 9: case 11:
+      if (day > 30) return false;
+      break;
+    case 2:
+      if (year % 4 != 0 || year % 400 != 0 || day > 28)
+        return false;
+      if (day > 29) return false;
+      break;
+  }
+  if (hour < 0 || hour > 23 ||
+      min < 0 || min > 59 ||
+      sec < 0 || sec > 59)
+    return false;
+  return true;
+}
+
+/**
+ * Checks if the given datetime field form a valid datetime
+ *
+ * If the datetime is invalid, prints an error message on stderr and terminates
+ * the program with exit status 2.
+ *
+ * @param year   The year of the datetime
+ * @param month  The month of the datetime
+ * @param day    The day of the datetime
+ * @param hour   The hour of the datetime
+ * @param min    The minutes of the datetime
+ * @param sec    The seconds of the datetime
+ */
+void datetime_validate(int year, int month, int day,
+                       int hour, int min, int sec) {
+  if (!datetime_is_valid(year, month, day, hour, min, sec)) {
+    fprintf(stderr,
+            "error: invalid datetime (%.4d-%.2d-%.2dT%.2d:%.2d:%.2d)\n", 
+            year, month, day, hour, min, sec);
+    exit(2);
+  }
+}
+
 void datetime_initialize(struct Datetime* datetime,
                          int year, int month, int day,
                          int hour, int min, int sec) {
+  datetime_validate(year, month, day, hour, min, sec);
   struct tm tm = {0};
   tm.tm_year = year - 1900;
   tm.tm_mon = month - 1;
