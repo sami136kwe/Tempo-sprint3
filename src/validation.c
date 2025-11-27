1e4d5889b04f9cb21681f651fc8bd8df2674583f
Alexandre Blondin Massé <blondin_masse.alexandre@uqam.ca>
2025-11-27 08:46:35 -0500

    Réusine validation dans tempo

--- src/validation.c
+++ src/validation.c
@@ -3,10 +3,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Memory
+// ------
+
 void report_memory_problem(const char* context) {
   fprintf(stderr, "error: memory allocation failed %s\n", context);
   exit(1);
 }
+
+// Datetimes
+// ---------
 
 void report_invalid_datetime(int year, int month, int day,
                              int hour, int min, int sec) {
@@ -19,4 +25,32 @@ void report_invalid_datetime(int year, int month, int day,
 void report_time_conversion_error(time_t t) {
   fprintf(stderr, "error: could not convert timestamp %ld to struct tm\n", t);
   exit(-1);
+}
+
+// CLI
+// ---
+
+void report_unrecognized_subcommand(const char* subcommand) {
+  fprintf(stderr, "error: unrecognized subcommand '%s'", subcommand);
+  exit(1);
+}
+
+void report_too_many_arguments(const char* subcommand) {
+  fprintf(stderr, "error: too many arguments to '%s' subcommand", subcommand);
+  exit(1);
+}
+
+void check_if_too_many_arguments(int argc, const char* subcommand) {
+  if (argc >= 3)
+    report_too_many_arguments(subcommand);
+}
+
+void report_wrong_option(const char* option) {
+  fprintf(stderr, "error: expected %s option\n", option);
+  exit(1);
+}
+
+void report_invalid_format_duration(const char* s) {
+  fprintf(stderr, "error: invalid duration format (%s)\n", s);
+  exit(1);
 }
--- src/validation.h
+++ src/validation.h
@@ -3,12 +3,18 @@
 
 #include <time.h>
 
+// Memory
+// ------
+
 /**
  * Reports a memory problem with a given context
  *
  * @param context  The context of the memory problem
  */
 void report_memory_problem(const char* context);
+
+// Datetimes
+// ---------
 
 /**
  * Reports that the given datetime fields form an invalid datetime
@@ -29,5 +35,44 @@ void report_invalid_datetime(int year, int month, int day,
  * @param t  The provided timestamp
  */
 void report_time_conversion_error(time_t t);
+
+// CLI
+// ---
+
+/**
+ * Reports that an unrecognized subcommand has been provided
+ *
+ * @param subcommand  The provided subcommand
+ */
+void report_unrecognized_subcommand(const char* subcommand);
+
+/**
+ * Reports that too many arguments have been provided
+ *
+ * @param subcommand  The provided subcommand
+ */
+void report_too_many_arguments(const char* subcommand);
+
+/**
+ * Checks if too many arguments have been provided
+ *
+ * @param argc        The number of arguments
+ * @param subcommand  The provided subcommand
+ */
+void check_if_too_many_arguments(int argc, const char* subcommand);
+
+/**
+ * Reports that some option was expected
+ *
+ * @param option  The expected option
+ */
+void report_wrong_option(const char* option);
+
+/**
+ * Reports that an invalid format duration was provided
+ *
+ * @param s  The string that was provided
+ */
+void report_invalid_format_duration(const char* s);
 
 #endif
