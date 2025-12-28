be370261f537bbbb1d3fa56cd892ed5b0d652f8a
Sami Geagea <kf691936@ens.uqam.ca>
2025-12-28 15:01:53 +0400

    amelioration de laffichage du graphe pour timerie.c

--- src/timeseries.c
+++ src/timeseries.c
@@ -233,23 +233,27 @@ void timeseries_print_observations(const struct Timeseries* timeseries) {
 }
 
 void timeseries_print_gnuplot_script(const struct Timeseries* timeseries) {
-  printf("set terminal png size 800,600\n");
-  printf("set output 'output.png'\n"); 
-  printf("set xlabel 'Time'\n");
-  printf("set ylabel 'Value'\n");
-  printf("set title 'Time Series'\n");
-  printf("set xdata time\n");
-  printf("set timefmt '%%Y-%%m-%%dT%%H:%%M:%%S'\n");
-  printf("set format x '%%Y-%%m-%%d\\n%%H:%%M:%%S'\n");
-  printf("set grid\n");
-  printf("set key off\n");
-  printf("plot '-' using 1:2 with linespoints\n");
+  printf("#!/usr/bin/gnuplot\n");
+  printf("# Data\n");
+  printf("$data << EOD\n");
+  
   for (size_t i = 0; i < timeseries->size; ++i) {
     struct Datetime datetime = datetime_copy(&timeseries->start_datetime);
     datetime_add_seconds(&datetime, timeseries->offsets[i]);
     printf("%s %d\n", datetime_to_rfc3339_string(&datetime),
-                      timeseries->values[i]);
+           timeseries->values[i]);
     datetime_delete(&datetime);
   }
-  printf("e\n");
+  
+  printf("EOD\n");
+  printf("# General settings\n");
+  printf("set terminal png\n");
+  printf("set xlabel \"Time\" offset 0,-1\n");
+  printf("set xdata time\n");
+  printf("set timefmt \"%%Y-%%m-%%dT%%H:%%M:%%S\"\n");
+  printf("set ylabel \"Value\"\n");
+  printf("set nokey\n");
+  printf("set output \"timeseries.png\"\n");
+  printf("# Plot times\n");
+  printf("plot $data using 1:2 ps 2.0 pt 7 lc \"orange\" title \"0.9\" with points\n");
 }
